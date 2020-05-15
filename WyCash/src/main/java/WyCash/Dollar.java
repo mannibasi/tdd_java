c82d2ae0b5cae3f61ccc51d88decb1042e957b65
mannibasi <manni.basi@gmail.com>
2020-05-15 06:57:17 +0100

    testMultiplication compilation errors fixed - test fails

---
+++ WyCash/src/main/java/WyCash/Dollar.java
@@ -0,0 +1,16 @@
+package WyCash;
+
+public class Dollar {
+
+	public int amount;
+
+	public Dollar(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void times(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
