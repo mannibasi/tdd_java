957c0eea3323d98a0d1bd26453314175bb518e7d
mannibasi <manni.basi@gmail.com>
2020-05-16 04:09:24 +0100

    Removed superfluous (Franc) multiplication tests

--- WyCash/src/test/java/wycash/MoneyTest.java
+++ WyCash/src/test/java/wycash/MoneyTest.java
@@ -40,12 +40,6 @@ public class MoneyTest {
     	assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
     
-    @Test public void testFrancMultiplication() {
-    	Money five = Money.franc(5);
-    	assertEquals(Money.franc(10), five.times(2));
-    	assertEquals(Money.franc(15), five.times(3));
-    }
-    
     @Test public void testCurrency() {
     	assertEquals("USD", Money.dollar(1).currency());
     	assertEquals("CHF", Money.franc(1).currency());
