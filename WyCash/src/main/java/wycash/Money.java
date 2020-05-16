a54d0bd8c0673fdc428122e091e7c48a4c1a3500
mannibasi <manni.basi@gmail.com>
2020-05-16 10:48:46 +0100

    Tidied up equals() implementation in-line with Sonar feedback

--- WyCash/src/main/java/wycash/Money.java
+++ WyCash/src/main/java/wycash/Money.java
@@ -1,25 +1,31 @@
 package wycash;
 
 public class Money {
-	
+
 	protected int amount;
 	protected String currency;
-	
+
 	public Money(int amount, String currency) {
 		this.amount = amount;
 		this.currency = currency;
 	}
-	
+
 	public Money times(int multiplier) {
 		return new Money(amount * multiplier, currency);
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+
+		if (this.getClass() != obj.getClass())
+			return false;
+
 		Money money = (Money) obj;
 		return amount == money.amount && currency().equals(money.currency());
 	}
-	
+
 	public String toString() {
 		return amount + " " + currency;
 	}
