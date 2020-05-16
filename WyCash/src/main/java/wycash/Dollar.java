37fe7483a59f99681c471f63a4af246e3b1fa25f
mannibasi <manni.basi@gmail.com>
2020-05-16 03:42:50 +0100

    Re-factored to use currency instance variable(s)

--- WyCash/src/main/java/wycash/Dollar.java
+++ WyCash/src/main/java/wycash/Dollar.java
@@ -7,7 +7,7 @@ public class Dollar extends Money {
 	}
 
 	public Money times(int multiplier) {
-		return new Dollar(amount * multiplier, "USD");
+		return new Dollar(amount * multiplier, currency);
 	}
 
 }
