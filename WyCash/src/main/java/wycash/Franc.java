f399f1e834848b15371044f8140d6c0ec97cccf8
mannibasi <manni.basi@gmail.com>
2020-05-16 03:52:19 +0100

    Reverted Franc times method to return Franc, not Money - to get back to green, so we can add further equality tests

--- WyCash/src/main/java/wycash/Franc.java
+++ WyCash/src/main/java/wycash/Franc.java
@@ -7,7 +7,7 @@ public class Franc extends Money {
 	}
 
 	public Money times(int multiplier) {
-		return new Money(amount * multiplier, currency);
+		return new Franc(amount * multiplier, currency);
 	}
 
 }
