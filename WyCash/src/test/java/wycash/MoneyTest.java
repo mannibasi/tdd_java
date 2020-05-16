7bb7e975f4af027327c1b2b35ec2fe40be77e785
mannibasi <manni.basi@gmail.com>
2020-05-16 03:05:23 +0100

    Further work/test changes to remove references to subclass Dollar - tests not running/compilation error(s) introduced as times not defined in Money

--- WyCash/src/test/java/wycash/MoneyTest.java
+++ WyCash/src/test/java/wycash/MoneyTest.java
@@ -29,7 +29,7 @@ public class MoneyTest {
 	//Compare Francs with Dollars
 	
     @Test public void testMultiplication() {
-    	Dollar five = Money.dollar(5);
+    	Money five = Money.dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
