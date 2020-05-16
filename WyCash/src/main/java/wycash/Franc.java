4ae5067e62ded70af49ae07277f2232c1592f971
mannibasi <manni.basi@gmail.com>
2020-05-16 03:36:15 +0100

    Re-factored to push up the constructor implementation(s)

--- WyCash/src/main/java/wycash/Franc.java
+++ WyCash/src/main/java/wycash/Franc.java
@@ -3,8 +3,7 @@ package wycash;
 public class Franc extends Money {
 	
 	public Franc(int amount, String currency) {
-		this.amount = amount;
-		this.currency = currency;
+		super(amount, currency);
 	}
 
 	public Money times(int multiplier) {
--- WyCash/src/main/java/wycash/Money.java
+++ WyCash/src/main/java/wycash/Money.java
@@ -7,6 +7,11 @@ public abstract class Money {
 	
 	public abstract Money times(int multiplier);
 	
+	public Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		Money money = (Money) obj;
