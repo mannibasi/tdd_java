ee34bab9b2351d96a6799be0f719a12367361121
mannibasi <manni.basi@gmail.com>
2020-05-17 19:25:33 +0100

    Re-factored to tidy up smell RE public fields/two levels of ref

--- WyCash/src/main/java/wycash/Bank.java
+++ WyCash/src/main/java/wycash/Bank.java
@@ -4,8 +4,7 @@ public class Bank {
 
 	public Money reduce(Expression source, String to) {
 		Sum sum = (Sum) source;
-		int amount = sum.augend.amount + sum.addend.amount;
-		return new Money(amount, to);
+		return sum.reduce(to);
 	}
 
 }
