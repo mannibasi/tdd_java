8a3e6b6e92b0aab62b60b89e0ebb6b3d9db7f861
mannibasi <manni.basi@gmail.com>
2020-05-17 20:02:58 +0100

    Fixed Money cannot be cast to Sum test failure - back to green

--- WyCash/src/main/java/wycash/Bank.java
+++ WyCash/src/main/java/wycash/Bank.java
@@ -3,6 +3,7 @@ package wycash;
 public class Bank {
 
 	public Money reduce(Expression source, String to) {
+		if (source instanceof Money) return (Money) source;
 		Sum sum = (Sum) source;
 		return sum.reduce(to);
 	}
