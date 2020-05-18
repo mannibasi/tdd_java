978e779795a7f2e4d2151289d1cf2c1af2f20cc6
mannibasi <manni.basi@gmail.com>
2020-05-18 21:54:40 +0100

    Added mixedAddition test, temporarily using Money vs Expression types and modified sum to ensure values being reduced

--- WyCash/src/test/java/wycash/MoneyTest.java
+++ WyCash/src/test/java/wycash/MoneyTest.java
@@ -98,4 +98,14 @@ public class MoneyTest {
 	public void testIdentityRate() {
 		assertEquals(1, new Bank().rate("USD", "USD"));
 	}
+	
+	@Test
+	public void testMixedAddition() {
+		Money fiveBucks = Money.dollar(5);
+		Money tenFrancs = Money.franc(10);
+		Bank bank = new Bank();
+		bank.addRate("CHF", "USD", 2);
+		Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+		assertEquals(Money.dollar(10), result);
+	}
 }
