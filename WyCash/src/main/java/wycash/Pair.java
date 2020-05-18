e0e1f3872e63dac7602f08ee4fa410801e4444a4
mannibasi <manni.basi@gmail.com>
2020-05-18 22:33:06 +0100

    Re-factored Pair's hashcode/equals to address code analysis issues

--- WyCash/src/main/java/wycash/Pair.java
+++ WyCash/src/main/java/wycash/Pair.java
@@ -10,12 +10,35 @@ public class Pair {
 		this.to = to;
 	}
 
-	public boolean equals(Object object) {
-		Pair pair = (Pair) object;
-		return from.equals(pair.from) && to.equals(pair.to);
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((from == null) ? 0 : from.hashCode());
+		result = prime * result + ((to == null) ? 0 : to.hashCode());
+		return result;
 	}
 
-	public int hashCode() {
-		return 0;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pair other = (Pair) obj;
+		if (from == null) {
+			if (other.from != null)
+				return false;
+		} else if (!from.equals(other.from))
+			return false;
+		if (to == null) {
+			if (other.to != null)
+				return false;
+		} else if (!to.equals(other.to))
+			return false;
+		return true;
 	}
+
 }
